400ff1d270b6153b91f6a2b83b8ee87e7f7ebccc
Naveen-Bhati <bhatin626@gmail.com>
2021-01-30 06:33:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  var [Sitem, setSitem] = useState("");
+  var [result, setResult] = useState([]);
+  var ChangeHandler = (e) => {
+    setSitem(e.target.value);
+  };
+  useEffect(() => {
+    async function autoSearch() {
+      var res = await axios.get(
+        `http://suggestqueries.google.com/complete/search?client=chrome&q=${Sitem}&hl=en`
+      );
+      var resultArr = res.data[1];
+      if (resultArr.length !== 0) {
+        setResult((result = resultArr));
+      }
+    }
+    autoSearch();
+  }, [Sitem]);
+
+  if (Sitem.length !== 0) {
+    document.getElementById("sItem").style.display = "flex";
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="searchBar">
+        <input
+          type="text"
+          onChange={ChangeHandler}
+          placeholder="Search Google"
+        />
+        <h1 id="sItem" className="sItem">
+          {Sitem}
+        </h1>
+
+        {Sitem.length !== 0 ? result.map((e) => <h1>{e}</h1>) : null}
+      </div>
     </div>
   );
 }
